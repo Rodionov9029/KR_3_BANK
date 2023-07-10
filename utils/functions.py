8e80b054f6a1a0e1e2ae315d9ceff698c16c1795
Rodionov9029 <rodionovrenat@gmail.com>
2023-07-10 22:20:42 +0400

    create logic

---
+++ utils/functions.py
@@ -0,0 +1,75 @@
+import json
+from utils import classes
+import os
+
+path_ = os.path.abspath('operations.json')
+DATA = json.load(open(os.path.join(path_), encoding='utf-8'))
+
+
+def executed_transaction():
+    """
+    Возвращает список из 5 последних выполненных операций, начиная с последней операции
+    """
+    # Создаем пустой список для всех выполненных транзакций
+    necessary_date = []
+    # Проходимся циклом по списку всех транзакций начиная с конца
+    for transaction in reversed(DATA):
+        # Если ключа 'state' нету в словаре (элементе списка), то пропускаем итерацию
+        if 'state' not in transaction.keys():
+            continue
+        # Если ключ 'state' равен значению 'EXECUTED', то добавляем этот элемент в список выполненных операций
+        if transaction['state'] == 'EXECUTED':
+            necessary_date.append(transaction)
+
+    # Возвращает список выполненных операций
+    return necessary_date
+
+
+def get_last_transaction():
+    """
+    Сортирует операции по дате и  возвращает 5 последних
+    """
+    list_ = executed_transaction()
+    return sorted(list_, key=lambda element: element['date'])[-1:-6:-1]
+
+
+def transaction_in_class():
+    """
+    Принимает список транзакций и возвращает список экземпляров класса Transaction
+    """
+    # Создаем пустой список для будущих экземпляров
+    list_ = get_last_transaction()
+    list_of_classes = []
+
+    # Проходимся циклом по последним транзакцим, задаем аргументы для экземпляра
+    for element in list_:
+        id_ = element['id']
+        state = element['state']
+        date = element['date']
+        amount = element['operationAmount']['amount']
+        name_currency = element['operationAmount']['currency']['name']
+        code_currency = element['operationAmount']['currency']['code']
+        description = element['description']
+        # Если есть счет отправителя
+        if 'from' in element.keys():
+            sender = element['from']
+        else:
+            sender = ''
+        recipient = element['to']
+
+        # Инициализируем экземпляр
+        transaction = classes.Transaction(id_, state, date, amount, name_currency, code_currency, description, sender,
+                                          recipient)
+        # Добавляем его в список
+        list_of_classes.append(transaction)
+    # Выводим список экземпляров
+    return list_of_classes
+
+
+def main():
+    """
+    Основная функция проекта
+    """
+    # Печать каждого элемента в списке классов
+    for transaction in transaction_in_class():
+        print(transaction)
